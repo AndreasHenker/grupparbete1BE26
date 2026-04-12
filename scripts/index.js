ed6c8bd5590da95fa7c9fb4fbed3dcc9efba74d1
Jeongsook Park <241577038+parkmolin@users.noreply.github.com>
2026-04-12 13:59:17 +0200

    finalVersion
    update 2026-04-12

---
+++ scripts/index.js
@@ -0,0 +1,127 @@
+const answers = document.getElementById("answers");
+const questionOptions = document.getElementsByClassName("answer-btn");
+const progress = document.getElementById("progress");
+const totalQuestions = document.getElementById("total");
+const questionText = document.getElementById("question");
+const nextBtn = document.getElementById("nextBtn");
+const scoreDisplay = document.getElementById("score");
+const finalResultContainer = document.getElementById("result");
+const quizContainer = document.getElementById("quiz-container");
+const result = document.getElementById("result");
+const resultScore = document.getElementById("result-score");
+const restartBtn = document.getElementById("restartBtn")
+const summary = document.getElementById("summary")
+
+function generateSummary(){
+    summary.innerHTML = "<h3>Detaljerat resultat:</h3>";
+
+    let feedBack = "";
+    for (let i = 0; i < quiz.trackRecord.length; i++) {
+        let record = quiz.trackRecord[i];
+        feedBack += `
+        <div class="summary-item">
+            <h5>Fråga ${i + 1}:</h5>
+            <h4>${quiz.questionBank[i].question}</h4>
+            <p> Ditt svar var: <strong>${record[0]===record[1] ? 'rätt! 🤩' : 'fel! 😩'}</strong>,</p>
+            <p style="color: #144301;">Rätt svar: <em style="color: #144301;">${quiz.questionBank[i].options[quiz.questionBank[i].correctIndex]}</em></p>
+        </div>`;
+    }
+    summary.innerHTML += feedBack;
+}
+
+function finalResult(){
+    //quizContainer.style.display = "none";
+    result.classList.remove("hidden");
+    quizContainer.classList.add("hidden");
+    
+    //här ska vi skapa resultatsidan
+    const correct = quiz.trackRecord.filter(r => r[0] === r[1]).length;
+    const total = quiz.questionBank.length;
+    const percent = Math.round((correct / total) * 100);
+
+    resultScore.textContent = `Du fick ${correct} av ${total} (${percent}%)`;
+    generateSummary();
+}
+
+function updateQuestion(){
+    questionText.textContent = quiz.questionBank[quiz.currentQuestion].question;
+    scoreDisplay.textContent = "Score: " + Math.round((quiz.score / quiz.questionBank.length) * 100) + "%";
+    totalQuestions.textContent = quiz.questionBank.length;
+    
+    const numberOfOptions = quiz.questionBank[quiz.currentQuestion].options.length;
+    for (let i = 0; i < numberOfOptions; i++) {
+            questionOptions[i].textContent = quiz.questionBank[quiz.currentQuestion].options[i];
+            questionOptions[i].classList.remove("correct","wrong")
+    }
+
+    progress.textContent = quiz.currentQuestion + 1;
+}
+
+function restartQuiz(){
+    quiz.newQuiz(); //nolställer quiz, så man kan återanvända det
+
+    for (let i = 0; i < 4; i++){
+            questionOptions[i].disabled = false;
+        }
+
+    result.classList.add("hidden");
+    quizContainer.classList.remove("hidden");
+    updateQuestion();
+   
+}
+
+function superQuiz(){
+    restartQuiz();
+    //next-button
+    answers.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        let answerGiven = event.submitter.textContent;
+        let correctAnswer = quiz.questionBank[quiz.currentQuestion].options[quiz.questionBank[quiz.currentQuestion].correctIndex];
+        if (answerGiven === correctAnswer){
+            console.log ("Rätt");
+            quiz.score ++;
+            event.submitter.classList.add("correct");
+        }
+        else {
+            console.log("Fel");
+            event.submitter.classList.add("wrong");
+            for (let i = 0; i < 4; i++){
+                if (questionOptions[i].textContent === correctAnswer) {
+                    questionOptions[i].classList.add("correct");
+                }
+            }
+        }
+        quiz.trackRecord.push([answerGiven, correctAnswer]);
+        quiz.questionBank[quiz.currentQuestion].answered = true;
+
+        nextBtn.disabled = false;
+        for (let i = 0; i < 4; i++){
+            questionOptions[i].disabled = true;
+        }
+    });
+
+    nextBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (quiz.currentQuestion < quiz.questionBank.length-1) {
+            quiz.currentQuestion++;
+            for (let i = 0; i < 4; i++){
+            questionOptions[i].disabled = false;
+            }
+        }
+
+        else {
+            scoreDisplay.textContent = "Score: " + Math.round((quiz.score / quiz.questionBank.length) * 100) + "%";
+            finalResult();
+        }
+        nextBtn.disabled = true;
+        updateQuestion();
+    });
+    //restart-button
+    restartBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        restartQuiz();
+    });
+}
+
+
+superQuiz();
